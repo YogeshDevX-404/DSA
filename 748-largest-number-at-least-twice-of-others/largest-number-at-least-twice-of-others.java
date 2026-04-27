500b3e8ff687d7b14c385b6f73c0d3858ede4132
Yogesh Prajapati <yogu4829@gmail.com>
2026-04-27 22:04:55 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (70.99%) - LeetSync

--- 748-largest-number-at-least-twice-of-others/largest-number-at-least-twice-of-others.java
+++ 748-largest-number-at-least-twice-of-others/largest-number-at-least-twice-of-others.java
@@ -1,27 +1,47 @@
 class Solution {
     public int dominantIndex(int[] nums) {
-        int n = nums.length;
+    //     int n = nums.length;
 
-        for (int i = 0; i < n; i++) {
+    //     for (int i = 0; i < n; i++) {
 
-            boolean isValid = true;
+    //         boolean isValid = true;
 
-            for (int j = 0; j < n; j++) {
+    //         for (int j = 0; j < n; j++) {
 
-                if (i == j) continue;
+    //             if (i == j) continue;
 
-                if (nums[i] < 2 * nums[j]) {
-                    isValid = false;
-                    break;
-                }
-            }
+    //             if (nums[i] < 2 * nums[j]) {
+    //                 isValid = false;
+    //                 break;
+    //             }
+    //         }
 
-            if (isValid) {
-                return i;
+    //         if (isValid) {
+    //             return i;
+    //         }
+    //     }
+
+    //     return -1;
+    // }
+
+
+
+             int idx = -1;
+        int max1 = -1;
+        int max2 = -1;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (nums[i] > max1) {
+                max2 = max1;
+                max1 = nums[i];
+                idx = i;
+
+            } else if (nums[i] > max2) {
+                max2 = nums[i];
             }
         }
 
-        return -1;
+        return (max1 >= 2 * max2) ? idx : -1;
     }
-    
 }
